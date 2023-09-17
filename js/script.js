0d8e0a85efde3aa0c7221449f64bdbc65b177bc2
Juaum <joaogpagnussati@gmail.com>
2023-09-17 00:21:02 -0300

    Clima

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const apiKey = "058b137b23a9d2646fe9a64396522027";
+const countryApiURL = "https://www.countryflagicons.com/FLAT/64/"
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
+const weatherIconElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const humidityElement = document.querySelector('#umidity span');
+const windElement = document.querySelector('#wind span');
+
+const weatherContainer = document.querySelector('#weather-data')
+
+const getWeatherData = async (city) => {
+    const apiWeahterURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeahterURL);
+    const data = await res.json();
+
+    return data;
+}
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute('src', countryApiURL + data.sys.country + '.png');
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}Km/h`;
+
+    weatherContainer.classList.remove('hide')
+};
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
+cityInput.addEventListener('keyup', (e) => {
+    if (e.code === 'Enter') {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+});
